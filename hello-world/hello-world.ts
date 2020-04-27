e84ea0723637ff55f3f539d603e4394582d10d1e
vreymond <valentinreymond@gmail.com>
2020-04-27 22:42:04 +0200

    [Dev/hello-world@feature] Hello world starter with deno

---
+++ hello-world/hello-world.ts
@@ -0,0 +1,11 @@
+// Accessing modules referenced as URLs
+import { serve } from "https://deno.land/std@v0.36.0/http/server.ts";
+
+// Server listening on port 8000
+const denoServer = serve({ port: 8000 });
+
+console.log("Hello world in terminal!");
+
+for await (const req of denoServer) {
+  req.respond({ body: "Hello World in browser!\n" });
+}
