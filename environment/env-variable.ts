6aebf1307948d068bdb68e5661429697866112e4
vreymond <valentinreymond@gmail.com>
2020-04-28 14:18:43 +0200

    [Dev/environment@update] Access .env file and execute commands in subprocessing

--- environment/env-variable.ts
+++ environment/env-variable.ts
@@ -1,14 +1,18 @@
 // Import the dotenv module from deno.land
 import { config } from "https://deno.land/x/dotenv/dotenv.ts";
 
+/* Access environment variable in the envConfig object
+   safe : true is a security check. If environment variables written in
+   .env.example are not given, the program will throw an error
+*/
 const envConfig = config({ safe: true })
 
+// Accessing ENV_VARIABLE from .env file
 console.log(envConfig.ENV_VARIABLE);
 
-// Subprocess commands
+// Subprocess commands in terminal
 const processCommands = Deno.run({
-    cmd: ["echo", "hello"]
+    cmd: envConfig.COMMAND.split(" "),
 });
 
 await processCommands.status();
-
